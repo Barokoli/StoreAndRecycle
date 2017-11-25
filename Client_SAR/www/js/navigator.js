edf74e1418edce4f32680020edd3a61fb873c08b
SebastianWitt <sebastian.witt@rwth-aachen.de>
2017-11-25 12:41:45 +0200

    Fixed Nav, included splash

--- Client_SAR/www/js/navigator.js
+++ Client_SAR/www/js/navigator.js
@@ -1,7 +1,9 @@
 var active_view = 0;
+var view_history = [];
 
 function load_view(nr) {
     $('#View_0'+active_view).hide();
+    view_history.push(active_view);
     $('#View_0'+nr).show();
     if ($("#View_0"+nr).html() == "") {
         $("#View_0"+nr).load("View_0"+nr+".html");
@@ -10,11 +12,11 @@ function load_view(nr) {
 }
 
 function init_nav() {
-  load_view(1);
+  load_view(0);
 }
 
 function nav_go_back() {
   if (active_view > 0) {
-    load_view(active_view - 1);
+    load_view(view_history.pop());
   }
 }

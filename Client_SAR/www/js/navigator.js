a7e75d96fce796df26f4ec17f1c014e3595f2143
SebastianWitt <sebastian.witt@rwth-aachen.de>
2017-11-25 22:46:45 +0200

    nav fix 2

--- Client_SAR/www/js/navigator.js
+++ Client_SAR/www/js/navigator.js
@@ -2,13 +2,11 @@ var active_view = 0;
 var view_history = [];
 
 function load_view(nr) {
-    var layer = nr > 9 ? "#View_" + nr : "#View_0" + nr;
-    var layer_a = nr > 9 ? "#View_" + active_view : "#View_0" + active_view;
-    $(layer_a).hide();
+    $('#View_0'+active_view).hide();
     view_history.push(active_view);
-    $(layer).show();
-    if ($(layer_a).html() == "") {
-        $(layer).load(layer+".html");
+    $('#View_0'+nr).show();
+    if ($("#View_0"+nr).html() == "") {
+        $("#View_0"+nr).load("View_0"+nr+".html");
     }
     active_view = nr;
 }
